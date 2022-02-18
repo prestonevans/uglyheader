3393f5d547f33ffd3b372781013fc1323f9fbe56
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-18 13:59:04 -0700

    ugly header init

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <nav>
+        <div className="left">
+          <a href="#">Whatever</a>
+        </div>
+        <div className="right">
+          <a href="#">meh</a>
+          <a href="#">meh</a>
+          <a href="#">meh</a>
+          <a href="#">meh</a>
+        </div>
+      </nav>
+    </header>
   );
 }
 
