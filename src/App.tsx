b1841b72da52e9d7becd7d3a7b559cf05ff1d998
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-18 14:48:07 -0700

    basic show hide

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <header>
-      <nav>
-        <div className="left">
-          <a href="#">Whatever</a>
-        </div>
-        <div className="right">
-          <a href="#">meh</a>
-          <a href="#">meh</a>
-          <a href="#">meh</a>
-          <a href="#">meh</a>
-        </div>
-      </nav>
-    </header>
-  );
+  const [sideMenu, setSideMenu ] = useState(false)
+
+  function hanldeHamburgerMenu() {
+    setSideMenu(!sideMenu);
+  }
+	return (
+		<div className="nono">
+			<header>
+				<nav>
+					<div className="left">
+						<a onClick={hanldeHamburgerMenu}>Whatever</a>
+					</div>
+					<div className="right">
+						<a href="#">meh</a>
+						<a href="#">meh</a>
+						<a href="#">meh</a>
+					</div>
+				</nav>
+			</header>
+      <div  className={sideMenu ? 'show' : 'hide'}>
+        <p>test1</p>
+        <p>test2</p>
+        <p>test3</p>
+        <p>test4</p>
+        <p>test5</p>
+      </div>
+      <section>
+        <h1>Welcome</h1>
+      </section>
+		</div>
+	);
 }
 
 export default App;
